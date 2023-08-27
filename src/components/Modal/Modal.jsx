aa48d340f9ffa9e803018d1a97359e4064e3e812
StanislavBobrivskyi <stanislavbobrivskyi@gmail.com>
2023-08-27 19:02:00 +0200

    refactoring modal component

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,40 +1,79 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+// import ReactModal from 'react-modal';
+// import { StyledModal, Overlay } from './modal.styled';
+
+// ReactModal.setAppElement('#root');
+
+// export class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   handleKeyDown = event => {
+//     if (event.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+
+//   render() {
+//     const { isOpen, onClose, largeImageUrl } = this.props;
+
+//     return (
+//       <StyledModal>
+//         <ReactModal
+//           isOpen={isOpen}
+//           onRequestClose={onClose}
+//           className="modal"
+//           overlayClassName="overlay"
+//         >
+//           <Overlay onClick={onClose}>
+//             <img src={largeImageUrl} alt="" />
+//           </Overlay>
+//         </ReactModal>
+//       </StyledModal>
+//     );
+//   }
+// }
+
+import React, { useEffect, useCallback } from 'react';
 import ReactModal from 'react-modal';
 import { StyledModal, Overlay } from './modal.styled';
 
 ReactModal.setAppElement('#root');
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+export const Modal = ({ isOpen, onClose, largeImageUrl }) => {
+  const handleKeyDown = useCallback(
+    event => {
+      if (event.code === 'Escape') {
+        onClose();
+      }
+    },
+    [onClose]
+  );
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [handleKeyDown]);
 
-  handleKeyDown = event => {
-    if (event.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
-
-  render() {
-    const { isOpen, onClose, largeImageUrl } = this.props;
-
-    return (
-      <StyledModal>
-        <ReactModal
-          isOpen={isOpen}
-          onRequestClose={onClose}
-          className="modal"
-          overlayClassName="overlay"
-        >
-          <Overlay onClick={onClose}>
-            <img src={largeImageUrl} alt="" />
-          </Overlay>
-        </ReactModal>
-      </StyledModal>
-    );
-  }
-}
+  return (
+    <StyledModal>
+      <ReactModal
+        isOpen={isOpen}
+        onRequestClose={onClose}
+        className="modal"
+        overlayClassName="overlay"
+      >
+        <Overlay onClick={onClose}>
+          <img src={largeImageUrl} alt="" />
+        </Overlay>
+      </ReactModal>
+    </StyledModal>
+  );
+};
